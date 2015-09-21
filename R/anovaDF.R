fc01e3c09d5e37f743ac0c25257e518d4914241e
moosik <vitalina@git>
2015-09-21 10:23:19 -0500

    minor language change

--- R/anovaDF.R
+++ R/anovaDF.R
@@ -1,21 +1,21 @@
 #' ANOVA overall P value for a linear model
-#' 
+#'
 #' The function calculates ANOVA overall P value for a linear
 #' model where y is a numeric vector (for example, an eigenvector
-#' calculated in an SVD) and x is a dependent variable from
+#' calculated by an SVD) and x is a dependent variable from
 #' the metadata. The function loops over the columns of the
 #' metadata
-#'  
+#'
 #'  @param vec vector of the response variable
 #'  @param df metadata
-#'  
+#'
 #'  @return a vector of P values for each model fitted where
-#'   input vector is the response variable and dependent 
+#'   input vector is the response variable and dependent
 #'   variable is on the of the columns in the provided
 #'   data frame
-#'  
+#'
 #'  @seealso code{\link{eigenVariable}}
-#'  
+#'
 #'  @export
 
 
@@ -24,8 +24,8 @@ anovaDF <- function(vec, df){
     stop("Vector length should be the same as the number of rows in a data frame")
   }
   else{
-    return(sapply(df, function(x) tryCatch({anova(lm(vec ~ x))$"Pr(>F)"[1]}, 
+    return(sapply(df, function(x) tryCatch({anova(lm(vec ~ x))$"Pr(>F)"[1]},
                                            error = function(e) {return(NA)},
                                            warning = function(w) {return(NA)})))
-  } 
+  }
 }
