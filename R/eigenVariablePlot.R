180f9dfa19d3d35e2e0c15cf8a91f5edb9e248c0
moosik <vitalina@git>
2015-09-21 12:56:19 -0500

    renamed varsInfluence, removed the original file

--- R/eigenVariablePlot.R
+++ R/eigenVariablePlot.R
@@ -1,9 +1,9 @@
-#' this function automatically makes a ggplot to 
+#' this function automatically makes a ggplot to
 #' show correlation between a principal component
 #' and a variable
 #' Provide number for the PC, provide the name of the variable
 #' Result is a plot
-#' require ggplot2 
+#' require ggplot2
 
 
 
@@ -13,15 +13,15 @@ varsInfluence <- function(pcs, variables, PC = 1, var.name){
   df.plot <- data.frame(pcs[, PC], variables[, var.name])
   colnames(df.plot) <- c(requested.pc, var.name)
   if(identical(class(df.plot[, var.name]), "numeric") | identical(class(df.plot[, var.name]), "integer")){
-    p <- ggplot(df.plot, aes_string(var.name, requested.pc)) + 
+    p <- ggplot(df.plot, aes_string(var.name, requested.pc)) +
       geom_point(shape = 20, size = 3, alpha = 0.6)
   }
   else if (identical(class(df.plot[, var.name]), "factor") | identical(class(df.plot[, var.name]), "character")){
     if (identical(class(df.plot[, var.name]), "character")){
       df.plot[, var.name] <- as.factor(df.plot[, var.name])
     }
-    p <- ggplot(df.plot, aes_string(var.name, requested.pc)) + 
-      geom_boxplot() + 
+    p <- ggplot(df.plot, aes_string(var.name, requested.pc)) +
+      geom_boxplot() +
       theme(axis.text.x = element_text(angle = 90, hjust = 1))
   }
   return(p)
