89b16fc2eb4a0bb691b97ee7c36ef26c2a5afe6f
moosik <vitalina@git>
2015-09-21 12:40:16 -0500

    added documentation to R/carefulKruskal.R

--- R/carefulKruskal.R
+++ R/carefulKruskal.R
@@ -1,3 +1,23 @@
+#' Calculate Kruskal-Wallis test between two variable, catch errors and warning
+#'
+#' Given names of a numeric variable \code{x1} and \code{x2} in a data frame \code{df}
+#' calculate the P value for the Chi-Square test between them. Use \code{tryCatch}
+#' to collect warnings and errors. Monte-Carlo simulation is performed by default
+#' for more accurate P values.
+#'
+#' @param num.vec.ind name of a numeric variable in a data frame \code{df}
+#' @param group.vec.ind name of a factor variable in a data frame \code{df}
+#' @param df data frame
+#'
+#' @return
+#'  \describe{
+#'    \item{"p.value"}{"P value or the error/warning message"}
+#'    \item{"statistic"}{"Test statistic or the error/warning message"}
+#'    \item{"test.type"}{"String indicating the test type: kruskal-wallis rank sum test"}
+#'  }
+#'
+#' @seealso \code{\link[stats]{kruskal.test}}
+
 carefulKruskal <- function(num.vec.ind, group.vec.ind, df){
   test.res <- tryCatch(kruskal.test(df[, num.vec.ind] ~ df[, group.vec.ind]),
                        error = function(e) return(e),
