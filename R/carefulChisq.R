70be2314a3f6692f37f00da81c7e8e8f5dc1ae10
moosik <vitalina@git>
2015-09-21 15:11:13 -0500

    cleaned up R/carefulChisq.R, regenerated the Rd file

--- R/carefulChisq.R
+++ R/carefulChisq.R
@@ -1,20 +1,21 @@
-#' Calculate Chi-square test between two variable, catch errors and warning
+#' Calculate Chi-square test between two variables, catch errors and warnings
 #'
-#' Given names of two variable \code{x1} and \code{x2} in a data frame \code{df}
-#' calculate the P value for the Chi-Square test between them. Use \code{tryCatch}
+#' Given names of two variables \code{x1} and \code{x2} in a data frame \code{df}
+#' calculate the P value for the Chi-square test between them. Use \code{tryCatch}
 #' to collect warnings and errors. Monte-Carlo simulation is performed by default
 #' for more accurate P values.
 #'
 #' @param x1 name of one factor variable in a data frame \code{df}
 #' @param x2 name of another factor variable in a data frame \code{df}
 #' @param df data frame
-#' @param sim.p.value if it TRUE (default) p value is computed for a Monte carlo test
+#' @param sim.p.value if it TRUE (default) P value is computed for a Monte carlo test
 #'
 #' @return
 #'  \describe{
-#'    \item{"p.value"}{"P value or the error/warning message"}
-#'    \item{"statistic"}{"Test statistic or the error/warning message"}
-#'    \item{"test.type"}{"String indicating the test type: chi-square test, monte carlo simulation"}
+#'    \item{p.value}{P value or the error/warning message}
+#'    \item{statistic}{Test statistic or the error/warning message}
+#'    \item{test.type}{String indicating the test type: chi-square test,
+#'    monte carlo simulation}
 #'  }
 #'
 #' @seealso \code{\link[stats]{chisq.test}}
