30d5cc3ea88538a33f365bfe40228ea5dabeb161
moosik <vitalina@git>
2015-09-22 12:26:02 -0500

    cleaned up documentation for t he summaryTable.R, updated the corresponding .Rd file

--- R/summaryTable.R
+++ R/summaryTable.R
@@ -1,18 +1,23 @@
 #' Summarize each column of a data frame
 #'
 #' Function takes a data frame and creates a series of summary statistics for each
-#' column of the data frame based on its class. If a column is numeric or integer,
-#' the function provides the minimum,
-#' median, mean, maximum, the number of the missing values and the class.
-#' If the class is factor it provides the number of levels, and the statistics for
-#' the number of cases as defined by each level: the minimum, the maximum,
+#' column. The type of statistics returned depends on the class of the column.
+#' If the column is numeric or integer,
+#' the function provides a minimum,
+#' median, mean, maximum and a number of missing cases and a class of the variable.
+#' If the class is a factor it provides the number of levels, and the statistics for
+#' the number of cases as defined by each level: the minimum,
 #' the median, the mean, the maximum and the number of missing cases. If the class
-#' is character then it is converted to factor and the statistics and the same
+#' is a character it is then converted to a factor and the same
 #' statistics are given.
 #'
+#' @param df a data frame for columns of which the summary statistics are to
+#' be calculated
+#'
 #' @return a data frame with seven columns: type of the variable, number of
-#' categories, minimum, mean, median, maximum, number of missing values. Row names
-#' of the data frame are the column names of the input data frame
+#' categories (when a factor or a character), minimum, mean, median, maximum,
+#' number of the missing cases. Row names
+#' of the data frame are the column names of the input data frame.
 #'
 #' @export
 
