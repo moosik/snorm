503082e7c2795b1c3d0db543b2fb91bc39e17daa
moosik <vitalina@git>
2016-03-03 19:03:49 -0600

    minor changes in geneOntology function

--- R/geneOntology.R
+++ R/geneOntology.R
@@ -8,12 +8,13 @@
 #' or equal to 0.05. Returns a data frame with GO categories.
 #'
 #' @param data a vector of ENTREZ Gene IDs.
-#' @param background a vector of the subset of genes which are used as the universe
+#' @param background a vector ENTREZ Gene IDs used as the universe
 #' for the hypergeometric calculation, also ENTREZ Gene IDs.
-#' @param annotation - a string giving the name of the annotation data package for
-#' the chip used to generate the data. See \code{\link[GOstats]{GOHyperGParams-class}}.
+#' @param annotation - a string giving the name of the annotation data package with
+#' GO annotation. See \code{\link[GOstats]{GOHyperGParams-class}}.
 #'
-#' @return data frame object with GO results. Columns:
+#' @return data frame object with GO results. Columns: Ontology (MF, BP or CC),
+#' GOID, Pvalue, OddsRatio, ExpCount, Count, Size, Term, P.Bonferroni
 #'
 #' @seealso \code{\link{fs}}
 #'
@@ -21,15 +22,16 @@
 
 
 gostats <- function(data, background, annotation){
+
+  # Test for Molecular function
   paramsMF <- new("GOHyperGParams", geneIds = data,
                   universeGeneIds = background, annotation = annotation,
                   ontology = "MF", pvalueCutoff = 1,
                   conditional = FALSE, testDirection = "over")
   hgOver <- hyperGTest(paramsMF)
   k <- p.adjust(pvalues(hgOver), method="bonferroni")
-  results.MF <- data.frame(Ontology = "MF", summary(hgOver, pvalue = 0.1),
-                           P.Bonferroni = k[pvalues(hgOver) <= 0.1])
-  results.MF <- results.MF[results.MF$P.Bonferroni <= 0.05,]
+  results.MF <- data.frame(Ontology = "MF", summary(hgOver, pvalue = 0.5),
+                           P.Bonferroni = k[pvalues(hgOver) <= 0.5])
   colnames(results.MF)[2] <- "GOID"
   paramsBP <- new("GOHyperGParams", geneIds = data,
                   universeGeneIds = background, annotation = annotation,
@@ -37,9 +39,8 @@ gostats <- function(data, background, annotation){
                   conditional = FALSE, testDirection = "over")
   hgOver <- hyperGTest(paramsBP)
   k <- p.adjust(pvalues(hgOver), method="bonferroni")
-  results.BP <- data.frame(Ontology = "BP", summary(hgOver, pvalue = 0.1),
-                           P.Bonferroni = k[pvalues(hgOver) <= 0.1])
-  results.BP <- results.BP[results.BP$P.Bonferroni <= 0.05,]
+  results.BP <- data.frame(Ontology = "BP", summary(hgOver, pvalue = 0.5),
+                           P.Bonferroni = k[pvalues(hgOver) <= 0.5])
   colnames(results.BP)[2] <- "GOID"
   paramsCC <- new("GOHyperGParams", geneIds = data,
                   universeGeneIds = background, annotation = annotation,
@@ -47,9 +48,8 @@ gostats <- function(data, background, annotation){
                   conditional = FALSE, testDirection = "over")
   hgOver <- hyperGTest(paramsCC)
   k <- p.adjust(pvalues(hgOver), method="bonferroni")
-  results.CC <- data.frame(Ontology = "CC", summary(hgOver, pvalue = 0.1),
-                           P.Bonferroni = k[pvalues(hgOver) <= 0.1])
-  results.CC <- results.CC[results.CC$P.Bonferroni <= 0.05,]
+  results.CC <- data.frame(Ontology = "CC", summary(hgOver, pvalue = 0.5),
+                           P.Bonferroni = k[pvalues(hgOver) <= 0.5])
   colnames(results.CC)[2] <- "GOID"
   results <- rbind(results.MF, results.BP, results.CC)
   results <- results[order(results$P.Bonferroni), ]
